19d494dc9ea53e703f45877bf1d1f83c64272ba8
Rose Robertson <rose@r.osey.me>
2019-04-08 14:09:31 -0700

    Update auth.js copy
    Update the copy when authenticating to have more helpful instructions,
    as per Zapier integration guidelines.

--- auth.js
+++ auth.js
@@ -5,7 +5,7 @@ const testAuth = (z) => {
 
   return promise.then((response) => {
     if (response.status === 401) {
-      throw new Error('The access token you supplied is invalid.');
+      throw new Error('The access token you supplied appears to be invalid.');
     }
     return response;
   });
@@ -20,8 +20,15 @@ module.exports = {
       label: 'Access Token',
       required: true,
       type: 'string',
+      helpText: 'Visit your [Account Preferences](https://app.getflow.com/preferences) to obtain your Personal Access Token. [Learn more…](https://developer.getflow.com/#getting-started)',
     },
-    { key: 'orgId', label: 'Organization ID', required: true, type: 'string' },
+    {
+      key: 'orgId',
+      label: 'Organization ID',
+      required: true,
+      type: 'string',
+      helpText: 'Once you have created your Personal Access Token in [Account Preferences](https://app.getflow.com/preferences), the Organization ID should be listed next to the token itself.',
+    },
   ],
   test: testAuth,
 };
